3e56fb48a9b624df1978d99c88a5f09af7e34ea4
Jose Garcia <jogarcia@tuta.io>
2020-09-30 15:07:40 -0400

    Add basic vue components

---
+++ resources/js/components/AppFooter.vue
@@ -0,0 +1,80 @@
+<template>
+  <footer>
+    <div class="jumbotron-fluid bg-dark text-white pt-5 footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-4 text-center">
+            <img
+              src="/images/kampeki-kun-name.png"
+              alt="Logo de ZaruArquitectura"
+              class="site-logo text-center"
+              style="width:80px;"
+            />
+            <p>
+              Natus eget occaecati, lobortis, vestibulum nam eros, risus lacinia
+              lacus.Lorem accusantium.
+            </p>
+          </div>
+          <div class="col-12 col-md-4">
+            <h3 class="padding-bottom-40 text-center">Contact details</h3>
+            <ul class="fa-ul margin-left-25">
+              <li class="padding-bottom-20 padding-left-5">
+                <span class="fa-li">
+                  <i class="fa fa-map-marker"></i>
+                </span>
+                125, Central Square,New York
+              </li>
+              <li class="padding-bottom-20 padding-left-5">
+                <span class="fa-li">
+                  <i class="fas fa-phone"></i>
+                </span>
+                91 123-456-7890/91
+              </li>
+              <li class="padding-bottom-20 padding-left-5">
+                <span class="fa-li">
+                  <i class="fa fa-mobile"></i>
+                </span>
+                (002) 123-4567
+              </li>
+              <li class="padding-bottom-20 padding-left-5">
+                <span class="fa-li">
+                  <i class="fa fa-envelope"></i>
+                </span>
+                Info@Example.Com
+              </li>
+            </ul>
+          </div>
+          <div class="col-12 col-md-4">
+            <h3 class="padding-bottom-40 text-center">Important links</h3>
+            <ul class="fa-ul margin-left-25">
+              <li class="padding-bottom-20 padding-left-5">
+                <a href = "/about" class="color-principal">
+                  <span class="fa-li">
+                    <i class="fa fa-link"></i>
+                  </span>
+                  About us
+                </a>
+              </li>
+              <li class="padding-bottom-20 padding-left-5">
+                <a href class="color-principal">
+
+                  <span class="fa-li">
+                    <i class="fa fa-link"></i>
+                  </span>
+                  Go to the contact page
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="text-center padding-top-30 padding-bottom-30">Copyright © 2020 Se no Kanpeki-Sa</div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "AppFooter"
+};
+</script>
---
+++ resources/js/components/VueSection.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="jumbotron jumbotron-fluid mb-0" :class="{ 'no-padding': !hasPadding }">
+    <div class="container"  >
+      <h1 v-if="title !== ''">{{ title }}</h1>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ""
+    },
+    hasPadding: {
+      type: Boolean,
+      default: true
+    }
+  },
+  name: "VueSection"
+};
+</script>
+
+<style scoped>
+.no-padding {
+    padding-bottom: 0px;
+    padding-top: 0px;
+}
+</style>
