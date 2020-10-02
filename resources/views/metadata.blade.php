30b9eab3a52af9141e1dec7f033f6bbd7d56bec9
Jose Manuel Cassola <jcassola@uclv.cu>
2020-10-01 20:36:06 -0400

    Include metadata file

--- resources/views/metadata.blade.php
+++ resources/views/metadata.blade.php
@@ -1,18 +1,18 @@
 {{-- Web Information Meta Tags --}}
 <meta property="og:title" content="Site Title" />
 <meta property="og:site_name" content="Site name"/>
-<meta property="og:description" content="Site description"/>
+<meta property="og:description" content="A short description of the site"/>
 <meta property="og:keywords" content="site, keywords"/>
 <meta property="og:author" content="John Doe"/>
 <meta property="og:type" content="website" />
-<meta property="og:url" content="Site url"/>
+<meta property="og:url" content="https:///www.site_url.com"/>
 <meta property="og:image" content="{{ asset('images/site_image.jpg') }}"/>
 <meta property="og:image:size" content="300" />
 <meta property="og:locale" content="es_ES" />
-<meta property="og:copyright" content="Site Copyright" />
+<meta property="og:copyright" content="Site Copyright 2020" />
 
 {{-- HTML5 default meta fields --}}
-<meta name="description" content="Site description">
+<meta name="description" content="A short description of the site">
 <meta name="keywords" content="site, keywords">
 <meta name="author" content="John Doe">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
